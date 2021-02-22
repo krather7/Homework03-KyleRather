695cc70ef5fcd186ece698cf823d2f86d7039391
krather7 <75811296+krather7@users.noreply.github.com>
2021-02-22 12:35:26 -0600

    Add files via upload
    Fixed error messages. Ready for turn in

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Assignment Code
+//--------------------------------------------------------------------------
+//Setting Variables
+//--------------------------------------------------------------------------
 var generateBtn = document.querySelector("#generate");
-var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var specialCharacter =['@',
+'%',
+'+',
+'\\',
+'/',
+"'",
+'!',
+'#',
+'$',
+'^',
+'?',
+':',
+',',
+')',
+'(',
+'}',
+'{',
+']',
+'[',
+'~',
+'-',
+'_',
+'.']
 var number = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -9,44 +34,43 @@ var password=[];
 var characters=0;
 var passwordLetter="";
 
-selectLowerCase=true;
-selectUpperCase=true;
-selectNumber=true;
-selectSpecialCharacter=true;
 
+//--------------------------------------------------------------------------
+//Setting Criteria Variables
+//--------------------------------------------------------------------------
+var selectLowerCase=true;
+var selectUpperCase=true;
+var selectNumber=true;
+var selectSpecialCharacter=true;
 
-// Write password to the #password input
+//--------------------------------------------------------------------------
+// Criteria Questions and Prompts
+//--------------------------------------------------------------------------
 function writePassword() {
   var characters = window.prompt("How many characters would you like your password to be? Chose between 8-128.");
   if (characters === null) {
     alert("You must enter a number between 8 and 128. Refresh page to try again.");
     return false;
 } else if (characters < 8 || characters > 128) {
-    window.confirm("You must enter a number between 8 and 128. Refresh page to try again.")
+    window.confirm("You must enter a number between 8 and 128.")
     return false;
   }
   else if (isNaN(characters)) {
-  window.confirm("You must enter a number between 8 and 128. Refresh page to try again.")
+  window.confirm("You must enter a number between 8 and 128.")
   return false;
   }
   
   characters=parseInt(characters)
   alert("You selected "+ characters + " characters.")
 
-  var selectLowerCase = window.confirm("Will there be lowercase leters?");
-  var selectUpperCase = window.confirm("Will there be uppercase letters");
-  var selectNumber = window.confirm("Will there be numbers?");
-  var selectSpecialCharacter = window.confirm("Will there be special characters?");
-
-  
+  var selectLowerCase = window.confirm("Will there be lowercase letters?");
   if (selectLowerCase === true) {
     charactersArray.push(...lowerCase);
     alert("You have confirmed lower case letters.");  }
     else {
       alert("You said no to lower case letters.")
     }
-
-
+  var selectUpperCase = window.confirm("Will there be uppercase letters");
   if (selectUpperCase === true) {
     charactersArray.push(...upperCase);
     alert("You have confirmed upper case letters.")
@@ -54,7 +78,7 @@ function writePassword() {
   else {
     alert("You said no to upper case letters.")
   }
-
+  var selectNumber = window.confirm("Will there be numbers?");
   if (selectNumber === true) {
     charactersArray.push(...number);
     alert("You have confirmed numbers.")
@@ -62,7 +86,7 @@ function writePassword() {
   else {
     alert("You said no to numbers.")
   }
-
+  var selectSpecialCharacter = window.confirm("Will there be special characters?");
   if (selectSpecialCharacter === true) {
     charactersArray.push(...specialCharacter);
     alert("You have confirmed special characters.")
@@ -70,11 +94,19 @@ function writePassword() {
   else {
     alert("You said no to special characters.")
   }
+  
+
 
   if (selectLowerCase === false && selectUpperCase === false && selectNumber === false && selectSpecialCharacter === false){
-    window.confirm("You must select at least one critera. Refresh page to try again.")
-    return false;
+    window.confirm("You must select at least one critera.")
+    location.reload();
+    return false; 
   }
+
+  //--------------------------------------------------------------------------
+  //Creating Password by getting length chosen by user, selecting random characters from the combined array, and adding it to the password variable, removing
+  //commas, and displaying it an alert.
+  //--------------------------------------------------------------------------
 
   for (i=0;i<characters;i++)
   {
@@ -84,6 +116,13 @@ function writePassword() {
 
   password = password.join("");
   alert("Your generated password is: " +password);
+ alert("Congratulations!");
+location.reload();
+
+
+  //--------------------------------------------------------------------------
+  //The rest is from the starter code
+  //--------------------------------------------------------------------------
   passwordText.value = password;
 
 }
